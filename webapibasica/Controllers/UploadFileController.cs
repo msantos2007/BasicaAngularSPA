a0c56d60537d1691a8222922f61da073fa39f6d2
msantos2007 <msantos2007@yahoo.com.br>
2022-06-03 13:49:11 -0300

    Mongo.GridFS

--- webapibasica/Controllers/UploadFileController.cs
+++ webapibasica/Controllers/UploadFileController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
 
 namespace webapibasica.Controllers
 {
     public class FileUploadResult
     {
         public long Length { get; set; }
-        public string Name { get; set; } = "";       
+        public string Name { get; set; } = "";
     }
 
     [ApiController]
@@ -20,33 +23,35 @@ namespace webapibasica.Controllers
         [Route("ImageUpload")]
         public async Task<IActionResult> ImageUpload(List<IFormFile> files, int AlunoId)
         {
-                return BadRequest("Nanan");  
+            //return BadRequest("Nanan");  
 
-                ////https://stackoverflow.com/questions/4988436/mongodb-gridfs-with-c-how-to-store-files-such-as-images
-                //  var server = MongoServer.Create("mongodb://localhost:27020");
-                //  var database = server.GetDatabase("tesdb");
 
-                //  var fileName = "D:\\Untitled.png";
-                //  var newFileName = "D:\\new_Untitled.png";
-                //  using (var fs = new FileStream(fileName, FileMode.Open))
-                //  {
-                //     var gridFsInfo = database.GridFS.Upload(fs, fileName);
-                //     var fileId = gridFsInfo.Id;
+            ////Using new driver
+            ////http://mongodb.github.io/mongo-csharp-driver/2.2/reference/gridfs/uploadingfiles/
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase database;
+            GridFSBucket bucket;
+            database = client.GetDatabase("tesdb");
+            bucket = new GridFSBucket(database);
 
-                //     ObjectId oid= new ObjectId(fileId);
-                //     var file = database.GridFS.FindOne(Query.EQ("_id", oid));
+            //Upload
+            byte[] m_Bytes;
+            await using (var memoryStream = new MemoryStream())
+            {
+                files[0].OpenReadStream().CopyTo(memoryStream);
+                m_Bytes = memoryStream.ToArray();
+            }
 
-                //     using (var stream = file.OpenRead())
-                //     {
-                //        var bytes = new byte[stream.Length];
-                //        stream.Read(bytes, 0, (int)stream.Length);
-                //        using(var newFs = new FileStream(newFileName, FileMode.Create))
-                //        {
-                //          newFs.Write(bytes, 0, bytes.Length);
-                //        } 
-                //     }
-                //  }
+            var id = await bucket.UploadFromBytesAsync(files[0].FileName, m_Bytes);
 
+            ////Download
+            //var bytes = await bucket.DownloadAsBytesAsync(id);
+
+            return Ok(id);
+
+
+            // Using legacy
+            ////https://stackoverflow.com/questions/4988436/mongodb-gridfs-with-c-how-to-store-files-such-as-images
 
 
             //Half way
@@ -78,5 +83,21 @@ namespace webapibasica.Controllers
             //     return BadRequest(erro);            
             // }
         }
+
+        [HttpGet]
+        [Route("ImageList")]
+        public async Task<IActionResult> ImageList()
+        {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase database;
+            IMongoCollection<BsonDocument> collection;
+            database = client.GetDatabase("tesdb");
+            collection = database.GetCollection<BsonDocument>("fs.files");
+
+            var resp = await collection.Find(_ => true).ToListAsync();
+
+            return Ok(resp);
+        }
+
     }
 }
